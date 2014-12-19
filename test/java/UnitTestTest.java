d3f3073061b965088b9983bde3e271eb2a9055a4
Moonsunhee1 <mshee0505@gmail.com>
2014-12-19 16:25:11 +0900

    first commit:

--- test/java/UnitTestTest.java
+++ test/java/UnitTestTest.java
@@ -6,6 +6,12 @@ import static org.junit.Assert.*;
  * Created by ce416-07 on 2014. 10. 24..
  */
 public class UnitTestTest {
+    @Test
+    public void testsentenseSum() throws Exception {
+            UnitTest unitTest = new UnitTest();
+            String result = unitTest.sentenseSum("sun","hee");
 
+        assertEquals("sunhee",result);
 
+    }
 }
